0cc64f7c045c71fe07e51ae8a91ab37f89a1207b
조영우 <choyoungwoo9@naver.com>
2025-04-23 18:04:47 +0900

    feat: 전역 예외 핸들러 추가
    - DTO 유효성 검사 실패 예외(MethodArgumentNotValidException)처리
    - DTO 변환 실패 예외(HttpMessageNotReadableException)처리

---
+++ src/main/kotlin/me/youngwoo/limitedslot/global/GlobalExceptionHandler.kt
@@ -0,0 +1,33 @@
+package me.youngwoo.limitedslot.global
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class GlobalExceptionHandler {
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): ResponseEntity<Map<String, Any>> {
+        val errorMap =
+            ex.bindingResult.fieldErrors.associate { fieldError ->
+                fieldError.field to (fieldError.defaultMessage)
+            }
+        val response =
+            mapOf(
+                "validationErrors" to errorMap,
+            )
+        return ResponseEntity.badRequest().body(response)
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleJsonParseException(ex: HttpMessageNotReadableException): ResponseEntity<Map<String, Any>> {
+        val response =
+            mapOf(
+                "message" to "요청 형식 오류",
+            )
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response)
+    }
+}
